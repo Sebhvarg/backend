461721bf64b2d07c0f3b5fd3d23c9848df90f092
Sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2025-01-13 10:50:08 -0500

    Guía 23

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+ # Importe requests y json
+import requests
+import json
+
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,4 +10,28 @@ from django.http import HttpResponse
 def index(request):
     #  return HttpResponse("Hello, World!")
     #  return render(request, 'main/base.html')
-    return render(request, 'main/index.html')
+      # Arme el endpoint del REST API
+    current_url = request.build_absolute_uri()
+    url = current_url + '/api/v1/landing'
+
+     # Petición al REST API
+    response_http = requests.get(url)
+    response_dict = json.loads(response_http.content)
+    
+
+
+
+    print("Endpoint ", url)
+    print("Response ", response_dict)
+
+     # Respuestas totales
+    total_responses = len(response_dict.keys())
+    responses = response_dict.values()
+
+     # Objeto con los datos a renderizar
+    data = {
+         'title': 'Landing - Dashboard',
+         'total_responses': total_responses,
+         'responses': responses,
+     }
+    return render(request, 'main/index.html', data)
